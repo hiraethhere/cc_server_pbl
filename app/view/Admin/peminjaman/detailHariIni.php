f066ec892daeb7be38c68de2bb6bef1d9464fa7c
NaqibZuhair <naqibzih@gmail.com>
2025-12-05 16:21:10 +0700

    INSYA ALLAH SUDAH

--- app/view/Admin/peminjaman/detailHariIni.php
+++ app/view/Admin/peminjaman/detailHariIni.php
@@ -1,20 +1,24 @@
-<main class="flex-1 p-8 overflow-y-auto bg-[#F9FAFC]">
+<main class="flex-1 p-8 overflow-y-auto bg-background1">
     <!-- Breadcrumb -->
     <nav class="mb-6 text-sm flex">
-        <a href="/Admin/Peminjaman" class="text-[#1E68FB] hover:text-blue-700">Data Peminjaman Ruangan</a>
-        <span class="mx-2 text-gray-400">
-            <img src="/icon/arrow.svg" class="w-5 h-5">
+        <a href="/Admin/Peminjaman" class="text-blue-overlay hover:text-blue-700">Data Peminjaman Ruangan</a>
+        <span class="mx-2 text-dark-overlay6">
+            <div>
+                <?= icon('arrowRight', 'w-5 h-5') ?>
+            </div>
         </span>
-        <a href="Admin/Peminjaman" class="font-medium text-gray-900">Hari ini</a>
-        <span class="mx-2 text-gray-400">
-            <img src="/icon/arrow.svg" class="w-5 h-5">
+        <a href="/Admin/Peminjaman" class="text-blue-overlay hover:text-blue-700">Hari ini</a>
+        <span class="mx-2 text-dark-overlay6">
+            <div>
+                <?= icon('arrowRight', 'w-5 h-5') ?>
+            </div>
         </span>
         <span class="font-medium text-gray-900">Detail Booking</span>
     </nav>
 
-    <h2 class="text-2xl font-bold text-[#171E29]">Booking Hari ini</h2>
+    <h2 class="text-2xl font-bold text-dark-overlay">Booking Hari ini</h2>
 
-    <div class="flex flex-col gap-8 lg:grid lg:grid-cols-[3fr_2fr] lg:gap-8 mt-5">
+    <div class="flex flex-col gap-8 lg:grid lg:grid-cols-[3fr_2fr] lg:gap-8 mt-2">
         <div class="bg-background2 rounded-2xl shadow-lg p-6 md:p-8 w-full">
 
             <!-- Nama Ruangan -->
@@ -29,23 +33,23 @@
             <!-- Waktu Peminjaman -->
             <div class="mb-5 w-full">
                 <label class="block text-sm font-medium text-dark-overlay7 mb-2">Waktu Peminjaman</label>
-                <div class="flex flex-row justify-between gap-3 border border-[#5C616A] p-4 rounded-lg flex-wrap">
+                <div class="flex flex-row justify-between gap-3 border border-dark-overlay4 p-4 rounded-lg flex-wrap">
                     <div class="flex-1">
-                        <p class="mb-2">Tanggal</p>
+                        <p class="mb-2 text-dark-overlay7">Tanggal</p>
                         <span
                             class="block w-full px-3 py-2 bg-white border border-dark-overlay4 rounded-lg text-left">
                             5 September 2025
                         </span>
                     </div>
                     <div class="flex-1">
-                        <p class="mb-2">Jam Mulai</p>
+                        <p class="mb-2 text-dark-overlay7">Jam Mulai</p>
                         <span 
                             class="block w-full px-3 py-2 bg-white border border-dark-overlay4 rounded-lg text-left">
                             13:00
                         </span>
                     </div>
                     <div class="flex-1">
-                        <p class="mb-2">Jam Selesai</p>
+                        <p class="mb-2 text-dark-overlay7">Jam Selesai</p>
                         <span 
                             class="block w-full px-3 py-2 bg-white border border-dark-overlay4 rounded-lg text-left">
                             15:00
@@ -57,23 +61,23 @@
             <!-- Penanggung Jawab -->
             <div class="mb-5 w-full">
                 <p class="block text-sm font-medium text-dark-overlay7 mb-2">Penanggung Jawab</p>
-                <div class="flex flex-col justify-between gap-3 border border-[#5C616A] p-4 rounded-lg flex-wrap">
+                <div class="flex flex-col justify-between gap-3 border border-dark-overlay4 p-4 rounded-lg flex-wrap">
                     <div class="flex-1">
-                        <p class="mb-2">Nama</p>
+                        <p class="mb-2 text-dark-overlay7">Nama</p>
                         <span
                             class="block w-full px-3 py-2 bg-white border border-dark-overlay4 rounded-lg text-left">
                             Muhammad Reza Arifin
                         </span>
                     </div>
                     <div class="flex-1">
-                        <p class="mb-2">NIM</p>
+                        <p class="mb-2 text-dark-overlay7">NIM</p>
                         <span 
                             class="block w-full px-3 py-2 bg-white border border-dark-overlay4 rounded-lg text-left">
                             2407411000
                         </span>
                     </div>
                     <div class="flex-1">
-                        <p class="mb-2">Jurusan</p>
+                        <p class="mb-2 text-dark-overlay7">Jurusan</p>
                         <span 
                             class="block w-full px-3 py-2 bg-white border border-dark-overlay4 rounded-lg text-left">
                             Teknik Informatika dan Komputer
@@ -85,7 +89,7 @@
             <!-- Informasi Anggota -->
             <div class="mb-5 w-full">
                 <p class="block text-sm font-medium text-dark-overlay7 mb-2">Informasi Anggota</p>
-                <div class="flex flex-col justify-between gap-3 border border-[#5C616A] p-4 rounded-lg flex-wrap">
+                <div class="flex flex-col justify-between gap-3 border border-dark-overlay4 p-4 rounded-lg flex-wrap">
                     <div class="flex-1">
                         <span
                             class="block w-full px-3 py-2 bg-white border border-dark-overlay4 rounded-lg text-left">
@@ -116,15 +120,7 @@
                 </span>
             </div>
 
-            <!-- Tombol Aksi -->
-            <div class="flex gap-4 pt-6">
-                <button class="flex-1 bg-[#C90B0B] hover:bg-red-700 text-white font-medium py-2.5 rounded-lg transition hover:cursor-pointer">
-                Cancel Booking
-                </button>
-                <button class="flex-1 bg-[#38C55C] hover:bg-green-700 text-white font-medium py-2.5 rounded-lg transition hover:cursor-pointer">
-                Mulai Peminjaman
-                </button>
-            </div>
+            
         </div>
 
         <div class="order-1 lg:order-0 lg:col-span-1 space-y-6 lg:sticky">
@@ -224,7 +220,58 @@
                         </a>
                     </div>
                 </div>
+
+                <!-- Tombol Aksi -->
+                <div class="gap-4 pt-6 grid grid-cols-1">
+                    <div class="grid grid-cols-2 gap-8">
+                        <button onclick="konfirmasiCancel()" class="flex-1 bg-red1 hover:bg-red-700 text-white font-medium py-2.5 rounded-lg transition hover:cursor-pointer">
+                            Cancel Booking
+                        </button>
+                        <button onclick="konfirmasiMulai()" class="flex-1 bg-green1 hover:bg-green-700 text-white font-medium py-2.5 rounded-lg transition hover:cursor-pointer">
+                            Mulai Peminjaman
+                        </button>
+                    </div>
+                    
+                    <button class="flex-1 bg-blue-overlay hover:bg-blue-700 text-white font-medium py-2.5 rounded-lg transition hover:cursor-pointer">
+                    Selesaikan Peminjaman
+                    </button>
+                </div>
             </div>
         </div>
     </div>
 </main>
+
+<script>
+    function konfirmasiMulai() {
+        Modal.confirm(
+            'Mulai peminjaman',
+            'Anda yakin ingin memulai peminjaman?',
+            function() {
+            // ini yang benar untuk POST form, bukan window.location.href
+            // document.getElementById('bookingForm').submit();
+            },
+            {
+            icon: <?= json_encode(icon("calendar", "w-12 h-12 text-green1")) ?>,
+            confirmText: 'Mulai',
+            confirmClass: 'w-full px-6 py-2 bg-green1 text-white rounded-lg font-semibold hover:bg-green-700 transition hover:cursor-pointer',
+            cancelText: 'Batalkan'
+            }
+        );
+    }
+
+    function konfirmasiCancel() {
+    Modal.confirm(
+        'Cancel Booking',
+        'Apakah Anda yakin ingin membatalkan booking?',
+        function() {
+            window.location.href = '#';
+        },
+        {
+            icon: <?= json_encode(icon("crossCircle", "w-12 h-12 text-red1")) ?>,
+            confirmText: 'Ya',
+            confirmClass: 'w-full px-6 py-2 bg-red1 text-white rounded-lg font-semibold hover:bg-red-700 transition hover:cursor-pointer',
+            cancelText: 'Batalkan'
+        }
+    );
+}
+</script>
