118bba8773040d1a8f9c0bb9b81f99748a2ffe52
divanamecca <nadivatv@gmail.com>
2025-12-11 09:21:17 +0700

    foto 8 ruangan, deskripsi dan detail ruangan nyusul

--- app/database/seeds/s_20251119_04_roomSeeder.php
+++ app/database/seeds/s_20251119_04_roomSeeder.php
@@ -10,12 +10,21 @@ class s_20251119_04_roomSeeder extends Seeder {
             status, min_capacity, max_capacity,
             created_at, updated_at
             ) VALUES
-            (1, 1, 'Ruang Rapat Utama', 'DefaultRuangan.jpg','Ruang besar untuk rapat tingkat institusi. Dilengkapi projector dan AC.','Ruang rapat besar', 1, 'active', 2, 4, NOW(), NOW()),
-            (2, NULL, 'Ruang Dosen Jurusan', 'DefaultRuangan.jpg','Ruang khusus dosen untuk diskusi internal dan bimbingan mahasiswa.', 'Ruang dosen', 2, 'active', 5, 7, NOW(), NOW()),
-            (3, 1, 'Ruang Himpunan TIK', 'DefaultRuangan.jpg','Ruang Himpunan untuk presentasi dan workshop kapasitas menengah.','Ruang seminar', 1, 'active', 4, 6, NOW(), NOW()),
-            (4, 1, 'Ruang Eksekutif Mahasiswa', 'DefaultRuangan.jpg','Ruang kecil untuk rapat Eksekutif Mahasiswa.','Hidup BEM PNJ, Hidup Perlawanan!', 1, 'active', 3, 5, NOW(), NOW()),
-            (5, 1, 'Ruang Duta', 'DefaultRuangan.jpg','Ruangan besar khusus untuk para duta PNJ. Kapasitas hingga 6 orang.','Hidup Duta PNJ! Hidup Duta PNJ!', 3, 'active', 3, 6, NOW(), NOW()),
-            (6, 1, 'Ruang Rapat', 'DefaultRuangan.jpg',NULL,'Ruang rapat yang hanya bisa dibooking oleh eksternal', 2, 'spesial', 10, 20, NOW(), NOW());
+            (1, 1, 'Galeri Literasi', 'GaleriLiterasi1.jpg','Ruang besar untuk rapat tingkat institusi. Dilengkapi projector dan AC.','Ruang rapat besar', 1, 'active', 2, 4, NOW(), NOW()),
+            (2, 1, 'Lentera Edukasi', 'LenteraEdukasi1.jpg','Ruang besar untuk rapat tingkat institusi. Dilengkapi projector dan AC.','Ruang rapat besar', 1, 'active', 2, 4, NOW(), NOW()),
+            (3, 1, 'Pusat Prancis', 'PusatPrancis1.jpg','Ruang besar untuk rapat tingkat institusi. Dilengkapi projector dan AC.','Ruang rapat besar', 1, 'active', 2, 4, NOW(), NOW()),
+            (4, 1, 'Ruang Cendikia', 'RuangCendikia1.jpg','Ruang besar untuk rapat tingkat institusi. Dilengkapi projector dan AC.','Ruang rapat besar', 1, 'active', 2, 4, NOW(), NOW()),
+            (5, 1, 'Ruang Rapat', 'RuangRapat1.jpg','Ruang besar untuk rapat tingkat institusi. Dilengkapi projector dan AC.','Ruang rapat besar', 1, 'active', 2, 4, NOW(), NOW()),
+            (6, 1, 'Ruang Sinergi', 'RuangSinergi1.jpg','Ruang besar untuk rapat tingkat institusi. Dilengkapi projector dan AC.','Ruang rapat besar', 1, 'active', 2, 4, NOW(), NOW()),
+            (7, 1, 'Sudut Pustaka', 'SudutPustaka1.jpg','Ruang besar untuk rapat tingkat institusi. Dilengkapi projector dan AC.','Ruang rapat besar', 1, 'active', 2, 4, NOW(), NOW()),
+            (8, 1, 'Zona Interaktif', 'ZonaInteraktif1.jpg','Ruang besar untuk rapat tingkat institusi. Dilengkapi projector dan AC.','Ruang rapat besar', 1, 'active', 2, 4, NOW(), NOW()),
+
+            (9, 1, 'Ruang Rapat Utama', 'DefaultRuangan.jpg','Ruang besar untuk rapat tingkat institusi. Dilengkapi projector dan AC.','Ruang rapat besar', 1, 'active', 2, 4, NOW(), NOW()),
+            (10, NULL, 'Ruang Dosen Jurusan', 'DefaultRuangan.jpg','Ruang khusus dosen untuk diskusi internal dan bimbingan mahasiswa.', 'Ruang dosen', 2, 'active', 5, 7, NOW(), NOW()),
+            (11, 1, 'Ruang Himpunan TIK', 'DefaultRuangan.jpg','Ruang Himpunan untuk presentasi dan workshop kapasitas menengah.','Ruang seminar', 1, 'active', 4, 6, NOW(), NOW()),
+            (12, 1, 'Ruang Eksekutif Mahasiswa', 'DefaultRuangan.jpg','Ruang kecil untuk rapat Eksekutif Mahasiswa.','Hidup BEM PNJ, Hidup Perlawanan!', 1, 'active', 3, 5, NOW(), NOW()),
+            (13, 1, 'Ruang Duta', 'DefaultRuangan.jpg','Ruangan besar khusus untuk para duta PNJ. Kapasitas hingga 6 orang.','Hidup Duta PNJ! Hidup Duta PNJ!', 3, 'active', 3, 6, NOW(), NOW()),
+            (14, 1, 'Ruang Rapat', 'DefaultRuangan.jpg',NULL,'Ruang rapat yang hanya bisa dibooking oleh eksternal', 2, 'spesial', 10, 20, NOW(), NOW());
             ";
         $this->db->query($sql);
         $this->db->execute();
